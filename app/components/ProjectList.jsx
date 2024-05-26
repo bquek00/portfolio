d12ba5cacfcb683dcee9fec6ad65b99ef5b5ad57
Bryan Quek <66237902+bquek00@users.noreply.github.com>
2024-05-26 23:10:13 +0800

    Update ProjectList.jsx

--- app/components/ProjectList.jsx
+++ app/components/ProjectList.jsx
@@ -21,7 +21,7 @@ export default function ProjectList() {
         }} className='text-white gridContainer pb-5'>
             <ProjectCard image="/Edexia.png" name="Grade Pilot - Automated Marking" 
             desc="Developed a forward-thinking application using Next.js, focused on reducing manual grading efforts by automating the evaluation of assignments using a Large Language Learning Model"
-            live="https://gradepilotai.vercel.app/" github="https://github.com/bquek00/GradePilotAI" tools={["React", "Next.js", "Supabase", "PostgreSQL","TypeScript", "Vercel"]}
+            live="https://grade-pilot.vercel.app/" github="https://github.com/bquek00/GradePilotAI" tools={["React", "Next.js", "Supabase", "PostgreSQL","TypeScript", "Vercel"]}
             />
             <ProjectCard image="/Japanese.png" name="Japanese Learning and Quiz App" 
             desc="Developed a multifunctional Next.js application, utilizing Supabase and PostgreSQL, that enables users to search Japanese words, compile notes, and self-quiz to enhance language learning and retention"
